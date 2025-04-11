1e0d7c2bf03d66e19149cc4f25f16fbc9989b38e
Coding Junction <109920996+coding-junction@users.noreply.github.com>
2025-04-11 14:28:26 +0000

    Fix component export/import issues in Landing

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,9 +1,11 @@
 import { Feature } from "@/components/ui/feature-with-image-carousel";
 
-export default function About() {
+const About = () => {
   return (
     <div className="w-full">
       <Feature />
     </div>
   );
-}
+};
+
+export default About;
