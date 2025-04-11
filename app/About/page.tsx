99617466aa5ddaf320887763b0e558b2f19f6945
ranadebsaha <ranadebsaha2002@gmail.com>
2025-04-11 18:23:55 +0530

    error updated by rds 4

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,11 +1,9 @@
 import { Feature } from "@/components/ui/feature-with-image-carousel";
 
-function About() {
+export default function About() {
   return (
     <div className="w-full">
       <Feature />
     </div>
   );
 }
-
-export { About };
