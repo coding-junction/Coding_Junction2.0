2e5eb584b160539ee838c4fff2ef1b1ae9d01b99
Coding Junction <109920996+coding-junction@users.noreply.github.com>
2025-05-28 07:34:14 +0000

    --encrypt sanity.ts

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client';
 
 export const sanity = createClient({
-  projectId: 'ozbeszd1', // find this in sanity.json or your project dashboard
-  dataset: 'production',
-  apiVersion: '2023-01-01',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-01-01',
   useCdn: true,
 });
